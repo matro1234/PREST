0d57c6394e1683654bef1b5d9f40fa6814c3fac7
matro1234 <74157597+matro1234@users.noreply.github.com>
2025-03-13 10:25:09 -0400

    Update evaluador.route.ts

--- src/app/routes/evaluador.route.ts
+++ src/app/routes/evaluador.route.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import {
   createModelForm,
   createEvaluador,
+  createEvaluadorAdmin,
 } from "../controllers/evaluador.controller";
 import { isAuthenticated } from '../middlewares/index'
 
 export default (router: Router) => {
   router.post("/evaluador/model/:id_evaluador", isAuthenticated, createModelForm);
-  router.post("/evaluador", createEvaluador);
+  router.post("/evaluador/:id_evaluador", createEvaluador);
+  router.post("/evaluador/admin", createEvaluadorAdmin);
 };
